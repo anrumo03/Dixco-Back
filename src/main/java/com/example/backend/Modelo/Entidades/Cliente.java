9b84774303321f27c69370610103acf05d8b0c15
Isabella Cardozo <isabella.cardozom@uqvirtual.edu.co>
2024-04-05 22:17:48 -0500

    Se realizó las anotaciones a cada clase

--- src/main/java/com/example/backend/Modelo/Entidades/Cliente.java
+++ src/main/java/com/example/backend/Modelo/Entidades/Cliente.java
@@ -3,27 +3,37 @@ package com.example.backend.Modelo.Entidades;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.Date;
+import java.util.Set;
+
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Administrador {
+public class Cliente {
 
     @Id
-    private String idAdmin;
+    private String idCliente;
     @Column(length=100)
-    private String nombreUsuario;
+    private String nombre;
     @Column(length=100)
-    private String contrasenia;
+    private String apellido;
     @Column(length=100)
-    private String nivelAcceso;
+    private String direccion;
     @NotBlank
     @Email
     private String email;
+    @Column(length=10)
+    private String telefono;
+    private Date fechaNacimiento;
+    @ManyToMany(mappedBy = "usuarios")
+    private Set<ListaEspera> listasEspera;
+
 }
--- src/main/java/com/example/backend/Modelo/Entidades/Reserva.java
+++ src/main/java/com/example/backend/Modelo/Entidades/Reserva.java
@@ -1,10 +1,26 @@
 package com.example.backend.Modelo.Entidades;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Reserva {
 
-    private String isReserva;
-    private String fecha;
-    private Usuario usuario;
+    @Id
+    private String idReserva;
+    private Date fecha;
+    @ManyToOne
+    private Cliente cliente;
+    @ManyToOne
     private Mesa mesaReservada;
     private int cantidadPersonas;
     private String estado;
